f5d5e9c962eaede25f48d9cea1589374c928ed09
Thomas Biesaart <thomas.biesaart@outlook.com>
2015-11-07 19:41:53 +0100

    Add delete method for single entity

--- src/main/php/DataDo/Data/Repository.php
+++ src/main/php/DataDo/Data/Repository.php
@@ -143,6 +143,17 @@ class Repository
     }
 
     /**
+     * Delete an entity by it's idProperty value.
+     * @param mixed $id the id value
+     * @return mixed the entity
+     * @throws ErrorException
+     */
+    public function delete($id) {
+        $idName = ucfirst($this->idProperty->getName());
+        return $this->__call("deleteBy$idName", array($id));
+    }
+
+    /**
      * Call a dsl method and create it if it does not exist.
      * @param $method
      * @param $args
